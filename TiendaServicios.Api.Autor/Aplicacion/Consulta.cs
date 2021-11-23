bbb532741c0fa1886b3470052126344e28030c7d
JUAN PABLO GONZALEZ DAVID <juan.gonzalez@sistemasentry.com.co>
2021-11-23 10:07:14 -0500

    Add project files.

---
+++ TiendaServicios.Api.Autor/Aplicacion/Consulta.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using TiendaServicios.Api.Autor.Modelo;
+using TiendaServicios.Api.Autor.Persistencia;
+
+namespace TiendaServicios.Api.Autor.Aplicacion
+{
+    public class Consulta
+    {
+        public class ListaAutor : IRequest<List<AutorLibro>> { }
+
+        public class Manejador : IRequestHandler<ListaAutor, List<AutorLibro>>
+        {
+            private readonly ContextoAutor _contexto;
+            public Manejador(ContextoAutor contexto)
+            {
+                _contexto = contexto;
+            }
+
+            public async Task<List<AutorLibro>> Handle(ListaAutor request, CancellationToken cancellationToken)
+            {
+                return await _contexto.AutorLibros.ToListAsync();
+            }
+        }
+    }
+}
---
+++ TiendaServicios.Api.Autor/Aplicacion/ConsultaFiltro.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TiendaServicios.Api.Autor.Modelo;
+using TiendaServicios.Api.Autor.Persistencia;
+
+namespace TiendaServicios.Api.Autor.Aplicacion
+{
+    public class ConsultaFiltro
+    {
+        public class AutorUnico : IRequest<AutorLibro>
+        {
+            public string AutorGuid { get; set; }
+        }
+
+        public class Manejador : IRequestHandler<AutorUnico, AutorLibro>
+        {
+            private readonly ContextoAutor _contexto;
+            public Manejador(ContextoAutor contexto)
+            {
+                _contexto = contexto;
+            }
+            public async Task<AutorLibro> Handle(AutorUnico request, CancellationToken cancellationToken)
+            {
+                AutorLibro autor = await _contexto.AutorLibros.Where(a => a.AutorLibroGuid == request.AutorGuid).FirstOrDefaultAsync();
+                
+                if(autor == null)
+                {
+                    throw new Exception("No se encontró el autor"); 
+                }
+
+                return autor;
+            }
+        }
+    }
+}
