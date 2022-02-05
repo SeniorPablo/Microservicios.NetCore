03a682852a7153822032fe37dc2137f6edc7b89d
JUAN PABLO GONZALEZ DAVID <juan.gonzalez@sistemasentry.com.co>
2022-02-05 11:47:10 -0500

    Consultas de microservices en Middlerware

--- TiendaServicios.Api.Gateway/MessageHandler/LibroHandler.cs
+++ TiendaServicios.Api.Gateway/MessageHandler/LibroHandler.cs
@@ -1,18 +1,24 @@
 ﻿using Microsoft.Extensions.Logging;
+using System;
 using System.Diagnostics;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using TiendaServicios.Api.Gateway.InterfaceRemote;
+using TiendaServicios.Api.Gateway.LibroRemote;
 
 namespace TiendaServicios.Api.Gateway.MessageHandler
 {
     public class LibroHandler : DelegatingHandler
     {
         private readonly ILogger<LibroHandler> _logger;
+        private readonly IAutorRemote _autorRemote;
 
-        public LibroHandler(ILogger<LibroHandler> logger)
+        public LibroHandler(ILogger<LibroHandler> logger, IAutorRemote autorRemote)
         {
             _logger = logger;
+            _autorRemote = autorRemote;
         }
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
@@ -20,7 +26,22 @@ namespace TiendaServicios.Api.Gateway.MessageHandler
             Stopwatch tiempo = Stopwatch.StartNew();
             _logger.LogInformation("Inicia el request");
 
-            var response = await base.SendAsync(request, cancellationToken);
+            HttpResponseMessage response = await base.SendAsync(request, cancellationToken);
+
+            if(response.IsSuccessStatusCode)
+            {
+                string contenido = await response.Content.ReadAsStringAsync();
+                JsonSerializerOptions options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                LibroModeloRemote resultado = JsonSerializer.Deserialize<LibroModeloRemote>(contenido, options);
+                var responseAutor = await _autorRemote.GetAutor(resultado.AutorLibro ?? Guid.Empty);
+                if(responseAutor.resultado)
+                {
+                    AutorModeloRemote objetoAutor = responseAutor.autor;
+                    resultado.AutorData = objetoAutor;
+                    string resultadoStr = JsonSerializer.Serialize(resultado);
+                    response.Content = new StringContent(resultadoStr, System.Text.Encoding.UTF8, "application/json");
+                }
+            }
 
             _logger.LogInformation($"Este proceso se hizo en {tiempo.ElapsedMilliseconds}ms");
 
--- TiendaServicios.Api.Gateway/Startup.cs
+++ TiendaServicios.Api.Gateway/Startup.cs
@@ -5,6 +5,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using System;
+using TiendaServicios.Api.Gateway.ImplementRemote;
+using TiendaServicios.Api.Gateway.InterfaceRemote;
 using TiendaServicios.Api.Gateway.MessageHandler;
 
 namespace TiendaServicios.Api.Gateway
@@ -22,6 +25,12 @@ namespace TiendaServicios.Api.Gateway
         public void ConfigureServices(IServiceCollection services)
         {
             //services.AddControllers();
+            services.AddHttpClient("AutorService", config =>
+            {
+                config.BaseAddress = new Uri(Configuration["Services:Autor"]);
+            });
+
+            services.AddSingleton<IAutorRemote, AutorRemote>();
             services.AddOcelot().AddDelegatingHandler<LibroHandler>();
         }
 
